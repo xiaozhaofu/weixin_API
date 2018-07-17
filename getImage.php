31d311f0f2880f285855c4169e265baf5e886cd7
xiaozhaofu <xiaozhaofu@163.com>
2018-07-17 15:53:55 +0800

    添加注释

--- getImage.php
+++ getImage.php
@@ -11,12 +11,14 @@ function getImage($url,$save_dir='',$filename=''){
     // 根据url获取远程文件
     // 先使用PHP CURL从下载临时素材接口获取文件流，再使用file_put_contents()方法把文件流写入指定的目录文件
     $ch = curl_init();
+
     curl_setopt($ch,CURLOPT_TIMEOUT,500);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
     curl_setopt($ch,CURLOPT_URL,$url);
     $res = curl_exec($ch);
+
     curl_close($ch);
     // 把图片保存到指定目录下的指定文件
     file_put_contents($save_dir.$filename,$res);
